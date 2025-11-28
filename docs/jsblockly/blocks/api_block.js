008ea987e6a4ce8ca9e0760b6f99654eca5bd27d
yillkid <yillkid@gmail.com>
2025-11-28 14:40:02 +0800

    API bridge

--- docs/jsblockly/blocks/api_block.js
+++ docs/jsblockly/blocks/api_block.js
@@ -1,3 +1,4 @@
+// 積木 A: 呼叫 API
 Blockly.Blocks['call_api'] = {
   init: function() {
     this.appendDummyInput()
@@ -14,5 +15,58 @@ Blockly.Blocks['call_api'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
+    this.setTooltip("呼叫 API 並將回應存到全域變數");
+  }
+};
+
+// 積木 B: 取得 API 回應物件
+Blockly.Blocks['get_api_response'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("取得 API 回應");
+    this.setOutput(true, null);
+    this.setColour(230);
+    this.setTooltip("取得完整的 API 回應物件");
+  }
+};
+
+// 積木 C: 從 API 回應取得欄位
+Blockly.Blocks['get_api_field'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("從 API 回應取得")
+        .appendField(new Blockly.FieldDropdown([
+          ["LED 列 (row)", "led_position.row"],
+          ["LED 欄 (col)", "led_position.col"],
+          ["顏色 (color)", "color"],
+          ["情緒 (sentiment)", "sentiment"],
+          ["文字 (text)", "text"]
+        ]), "FIELD");
+    this.setOutput(true, null);
+    this.setColour(230);
+    this.setTooltip("從 API 回應中取得特定欄位的值");
+  }
+};
+
+// 積木 D: 呼叫 API 並執行 (statement block with callback)
+Blockly.Blocks['call_api_and_do'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("呼叫 API 並執行");
+    this.appendValueInput("TEXT")
+        .setCheck("String")
+        .appendField("文字");
+    this.appendValueInput("X")
+        .setCheck("Number")
+        .appendField("位置 X");
+    this.appendValueInput("Y")
+        .setCheck("Number")
+        .appendField("位置 Y");
+    this.appendStatementInput("DO")
+        .appendField("執行");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("呼叫 API，等待回應後執行內部程式碼");
   }
 };
--- docs/jsblockly/generators/api_block.js
+++ docs/jsblockly/generators/api_block.js
@@ -2,12 +2,13 @@
 // 需要使用 javascriptGenerator 而不是 Blockly.JavaScript
 const javascriptGenerator = Blockly.JavaScript || window.Blockly.JavaScript;
 
+// 積木 A: 呼叫 API (statement block)
 javascriptGenerator.forBlock['call_api'] = function(block, generator) {
   var text = generator.valueToCode(block, 'TEXT', generator.ORDER_ATOMIC) || "''";
   var x = generator.valueToCode(block, 'X', generator.ORDER_ATOMIC) || '0';
   var y = generator.valueToCode(block, 'Y', generator.ORDER_ATOMIC) || '0';
 
-  var code = 'fetch("https://your-api-endpoint/emotion", {\n' +
+  var code = 'fetch("https://river.4impact.cc/wheel", {\n' +
     '  method: "POST",\n' +
     '  headers: { "Content-Type": "application/json" },\n' +
     '  body: JSON.stringify({\n' +
@@ -18,6 +19,58 @@ javascriptGenerator.forBlock['call_api'] = function(block, generator) {
     '.then(res => res.json())\n' +
     '.then(data => {\n' +
     '  console.log("API 回傳:", data);\n' +
+    '  window.apiResponse = data;\n' +
+    '})\n' +
+    '.catch(error => {\n' +
+    '  console.error("API 錯誤:", error);\n' +
+    '});\n';
+
+  return code;
+};
+
+// 積木 B: 取得 API 回應物件 (value block - 回傳整個 object)
+javascriptGenerator.forBlock['get_api_response'] = function(block, generator) {
+  var code = 'window.apiResponse || {}';
+  return [code, generator.ORDER_MEMBER];
+};
+
+// 積木 C: 從 API 回應取得欄位 (value block - 回傳特定欄位的值)
+javascriptGenerator.forBlock['get_api_field'] = function(block, generator) {
+  var field = block.getFieldValue('FIELD');
+
+  // 處理巢狀屬性，例如 "led_position.row"
+  var accessPath = field.split('.').map(function(part) {
+    return '["' + part + '"]';
+  }).join('');
+
+  // 使用 !== undefined 來正確處理 0、false、空字串等 falsy 值
+  var code = '(window.apiResponse' + accessPath + ' !== undefined ? window.apiResponse' + accessPath + ' : null)';
+  return [code, generator.ORDER_CONDITIONAL];
+};
+
+// 積木 D: 呼叫 API 並執行 (statement block with callback)
+javascriptGenerator.forBlock['call_api_and_do'] = function(block, generator) {
+  var text = generator.valueToCode(block, 'TEXT', generator.ORDER_ATOMIC) || "''";
+  var x = generator.valueToCode(block, 'X', generator.ORDER_ATOMIC) || '0';
+  var y = generator.valueToCode(block, 'Y', generator.ORDER_ATOMIC) || '0';
+  var statements = generator.statementToCode(block, 'DO');
+
+  var code = 'fetch("https://river.4impact.cc/wheel", {\n' +
+    '  method: "POST",\n' +
+    '  headers: { "Content-Type": "application/json" },\n' +
+    '  body: JSON.stringify({\n' +
+    '    text: ' + text + ',\n' +
+    '    position: [' + x + ', ' + y + ']\n' +
+    '  })\n' +
+    '})\n' +
+    '.then(res => res.json())\n' +
+    '.then(data => {\n' +
+    '  console.log("API 回傳:", data);\n' +
+    '  window.apiResponse = data;\n' +
+    statements +
+    '})\n' +
+    '.catch(error => {\n' +
+    '  console.error("API 錯誤:", error);\n' +
     '});\n';
 
   return code;
